96e667d0dff90ee5afe010dbd6205b4be82613c9
Sona <sonie.zeynalova@gmail.com>
2026-07-11 15:50:02 +0400

    add page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
 import MovieCard from './Components/MovieCard.jsx'
 import Home from './Pages/Home.jsx'
+import { Routes, Route } from 'react-router-dom'
+import Favorites from './Pages/Favorites.jsx'
+import NavBar from './Components/NavBar.jsx'
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />}/> 
+          <Route path='/favorites' element={<Favorites />}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 
